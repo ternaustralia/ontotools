7f4045806cdd42e2f3d66719ae5c25063276e398
Edmond Chuc <e.chuc@uq.edu.au>
2022-04-21 04:45:35 +0000

    Fix version command not exiting with error status code.

--- ontotools/commands/version.py
+++ ontotools/commands/version.py
@@ -1,3 +1,5 @@
+import sys
+
 import typer
 
 from ontotools.logging import logger
@@ -9,21 +11,21 @@ app = typer.Typer()
 @app.command()
 def compare(
     version: str = typer.Argument(..., help="The version to compare"),
-    filename: str = typer.Argument(
-        ..., help="The Turtle file with the version info"
-    ),
+    filename: str = typer.Argument(..., help="The Turtle file with the version info"),
 ):
     """Exits with status code 1 if the version is not the same or version info is not found."""
 
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
         result = compare_func(version, content)
 
         if result is None:
             logger.error("No version info found.")
-        elif result == False:
+            sys.exit(1)
+        elif result is False:
             logger.error("The version is not the same.")
-        elif result == True:
+            sys.exit(1)
+        elif result is True:
             logger.info("The version is the same.")
         else:
             raise RuntimeError(f"Unexpected result. Got {type(result)}.")
