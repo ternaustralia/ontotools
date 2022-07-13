e4ae927126b69de29d9ba3c21f9ec8e520601c35
junrongYu <junrong.yu@uq.edu.au>
2022-07-13 05:49:15 +0000

    Bound new prefixes `urnc` and `urnp`

--- ontotools/graph.py
+++ ontotools/graph.py
@@ -2,10 +2,14 @@ from rdflib import Graph, Namespace
 
 
 TERN = Namespace("https://w3id.org/tern/ontologies/tern/")
+URNC = Namespace("urn:class:")
+URNP = Namespace("urn:property:")
 
 
 def serialize(graph: Graph) -> str:
     # Reading files overrides pre-bound prefixes in RDFLib graphs.
     # This serialize function will bind the prefixes before serializing
     graph.bind("tern", TERN)
+    graph.bind("urnc", URNC)
+    graph.bind("urnp", URNP)
     return graph.serialize(format="longturtle")
