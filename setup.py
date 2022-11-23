c0a57ec7640df4c3112424fa0eb282d63321f97e
Edmond Chuc <e.chuc@uq.edu.au>
2022-11-23 01:46:00 +0000

    Add install_requires. Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ontotools",
-    version="0.6.2",
+    version="0.7.0-post1",
     author="Edmond Chuc",
     author_email="e.chuc@uq.edu.au",
     description="Python ontology tools.",
@@ -24,5 +24,11 @@ setuptools.setup(
     ],
     python_requires=">=3.8",
     entry_points={"console_scripts": ["ontotools = ontotools.cli:app"]},
-    install_requires=["rdflib>=6.1.1,<7.0.0", "typer>=0.4.0,<1.0.0"],
+    install_requires=[
+        "rdflib>=6.1.1,<7.0.0",
+        "typer>=0.4.0,<1.0.0",
+        "httpx>=0.23.0,<0.24.0",
+        "jinja2>=3.1.2,<3.2.0",
+        "rich>=12.6.0,<12.7.0",
+    ],
 )
